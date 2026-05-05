f1373a1e62cc3cd894ddeb8fd591ad49851a6c8a
mohanreddy111 <viswa4912@gmail.com>
2026-05-05 16:43:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, KrishnaVcube12345");
      System.out.println("Hello, devopsbbykrishna21");
-     System.out.println("Hello, devopsbykrishna"); 
+     System.out.println("Hello, reddy1"); 
 
   }
 }
